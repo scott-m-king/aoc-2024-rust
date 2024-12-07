c2813421694690d6f7a460b5378a5c4182c20b61
Scott <scott.m.king@outlook.com>
2024-12-07 15:40:25 -0800

    Day 2 start

--- src/day2.rs
+++ src/day2.rs
@@ -1,9 +1,36 @@
-fn parse_input(input: &str)  {
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    input
+        .split("\n")
+        .map(|x| {
+            x.split_whitespace()
+                .filter_map(|y| y.parse::<i32>().ok())
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>()
+}
 
+trait SafetyFeatures {
+    fn is_sorted(&self) -> bool;
+    fn is_safe(&self) -> bool;
+}
+
+impl SafetyFeatures for &Vec<i32> {
+    fn is_sorted(&self) -> bool {
+        self.is_sorted_by(|x, y| x.cmp(y).is_gt()) || self.is_sorted_by(|x, y| x.cmp(y).is_lt())
+    }
+
+    fn is_safe(&self) -> bool {
+        self.windows(2)
+            .all(|window| (1..=3).contains(&(window[0] - window[1]).abs()))
+    }
 }
 
 pub fn part1(input: &str) -> i32 {
-    0
+    parse_input(input)
+        .iter()
+        .map(|line| line.is_sorted() && line.is_safe())
+        .filter(|x| *x)
+        .count() as i32
 }
 
 pub fn part2(input: &str) -> i32 {
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 mod day2;
 
 fn main() {
-    let arg = utils::load_file("data/day-2-test.txt");
+    let arg = utils::load_file("data/day-2.txt");
     println!("Part 1: {}", day2::part1(&arg));
     println!("Part 2: {}", day2::part2(&arg));
 }
