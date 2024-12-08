0af8efeb9dab1e28461876a7ff70ad2a173096c4
Scott <scott.m.king@outlook.com>
2024-12-07 16:01:16 -0800

    Day 2 finish

--- src/day2.rs
+++ src/day2.rs
@@ -14,14 +14,14 @@ trait SafetyFeatures {
     fn is_safe(&self) -> bool;
 }
 
-impl SafetyFeatures for &Vec<i32> {
+impl SafetyFeatures for Vec<i32> {
     fn is_sorted(&self) -> bool {
         self.is_sorted_by(|x, y| x.cmp(y).is_gt()) || self.is_sorted_by(|x, y| x.cmp(y).is_lt())
     }
 
     fn is_safe(&self) -> bool {
         self.windows(2)
-            .all(|window| (1..=3).contains(&(window[0] - window[1]).abs()))
+            .all(|w| (1..=3).contains(&(w[0] - w[1]).abs()))
     }
 }
 
@@ -34,5 +34,18 @@ pub fn part1(input: &str) -> i32 {
 }
 
 pub fn part2(input: &str) -> i32 {
-    0
+    parse_input(input)
+        .into_iter()
+        .map(|line| {
+            (0..line.len()).any(|i| {
+                let list_with_index_removed: Vec<i32> = line
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(j, k)| if i != j { Some(*k) } else { None })
+                    .collect();
+                list_with_index_removed.is_sorted() && list_with_index_removed.is_safe()
+            })
+        })
+        .filter(|x| *x)
+        .count() as i32
 }
