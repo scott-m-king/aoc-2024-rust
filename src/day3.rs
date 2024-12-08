4cf17e42f8acdf4e0f12da441ea5bc5275b212a8
Scott <scott.m.king@outlook.com>
2024-12-08 01:50:44 -0800

    Day 3 finish

--- src/day3.rs
+++ src/day3.rs
@@ -16,30 +16,44 @@ fn parse_input(input: &str) -> Vec<(i32, i32)> {
 }
 
 pub fn part1(input: &str) -> i32 {
-    parse_input(input)
-        .iter()
-        .fold(0, |acc, (l, r)| acc + (l * r))
+    parse_input(input).iter().map(|(l, r)| l * r).sum()
+}
+
+fn get_by_regex<'a>(re: &'a str, input: &'a str, action: &'a str) -> Vec<(&'a str, usize)> {
+    Regex::new(re)
+        .unwrap()
+        .find_iter(input)
+        .map(|m| (action, m.start()))
+        .collect::<Vec<(&str, usize)>>()
 }
 
 pub fn part2(input: &str) -> i32 {
-    let deactivations = Regex::new(r"don't\(\)")
-        .unwrap()
-        .find_iter(input)
-        .map(|m| m.end())
-        .collect::<Vec<usize>>();
+    let mut activations = get_by_regex(r"do\(\)", input, "activate");
+    let deactivations = get_by_regex(r"don't\(\)", input, "deactivate");
 
-    let reactivations = Regex::new(r"do\(\)")
-        .unwrap()
-        .find_iter(input)
-        .map(|m| m.end())
-        .collect::<Vec<usize>>();
+    activations.insert(0, ("activate".into(), 0));
+    activations.extend(deactivations);
+    activations.sort_by(|(_, a), (_, b)| a.cmp(b));
 
-    deactivations
+    let mut curr = "start";
+    let mut filtered: Vec<usize> = activations
         .iter()
-        .zip(reactivations.iter())
-        .for_each(|(&l, &r)| {
-            let substring = &input[l..r];
-            println!("{:?}", substring);
-        });
-    0
+        .filter_map(|(action, idx)| {
+            if curr == "start" || *action != curr {
+                curr = action;
+                Some(*idx)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    filtered.push(input.len() - 1);
+
+    filtered
+        .windows(2)
+        .step_by(2)
+        .flat_map(|w| parse_input(&input[w[0]..w[1]]))
+        .map(|(l, r)| l * r)
+        .sum()
 }
