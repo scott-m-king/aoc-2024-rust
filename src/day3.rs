74da2f2379d5cb9387d79a9db4a20bce4e8b49a0
Scott <scott.m.king@outlook.com>
2024-12-07 16:58:30 -0800

    Day 3 pt 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,9 +1,24 @@
-fn parse_input(input: &str) {
+use regex::Regex;
 
+fn parse_input(input: &str) -> Vec<(i32, i32)> {
+    Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)")
+        .unwrap()
+        .find_iter(input)
+        .map(|m| m.as_str())
+        .map(|line| {
+            if let (Some(start), Some(end)) = (line.find("("), line.find(")")) {
+                let result: &Vec<&str> = &line[start + 1..end].split(",").collect();
+                return (result[0].parse().unwrap(), result[1].parse().unwrap());
+            }
+            (0, 0)
+        })
+        .collect::<Vec<(i32, i32)>>()
 }
 
 pub fn part1(input: &str) -> i32 {
-    0
+    parse_input(input)
+        .iter()
+        .fold(0, |acc, (l, r)| acc + (l * r))
 }
 
 pub fn part2(input: &str) -> i32 {
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 mod day3;
 
 fn main() {
-    let arg = utils::load_file("data/day-3-test.txt");
+    let arg = utils::load_file("data/day-3.txt");
     println!("Part 1: {}", day3::part1(&arg));
     println!("Part 2: {}", day3::part2(&arg));
 }
