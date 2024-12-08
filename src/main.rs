6dc0bf8f312a53c9eaabb44d2173380ecbdcb0ad
Scott <scott.m.king@outlook.com>
2024-12-07 16:10:19 -0800

    Day 3 start

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 mod utils;
-mod day2;
+mod day3;
 
 fn main() {
-    let arg = utils::load_file("data/day-2.txt");
-    println!("Part 1: {}", day2::part1(&arg));
-    println!("Part 2: {}", day2::part2(&arg));
+    let arg = utils::load_file("data/day-3-test.txt");
+    println!("Part 1: {}", day3::part1(&arg));
+    println!("Part 2: {}", day3::part2(&arg));
 }
